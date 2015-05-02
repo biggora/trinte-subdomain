8217b23ec76cb2ad7a6170fdb976ed409d01bc49
Alexey Gordeyev <aleksej@gordejev.lv>
2015-05-02 13:38:12 +0300

    Init subdomain module

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ *  TrinteJS Subdomain Middleware
+ *
+ *  @project     TrinteJS
+ *  @version     0.0.1
+ *  @package     trinte-subdomain
+ *  @author      Aleksejs Gordejevs
+ *  @created     2015-05-02 03:12:02
+ * 
+ *  Created by init script
+ *  App based on TrinteJS MVC framework
+ *  TrinteJS homepage http://www.trintejs.com
+ **/
+
+module.exports = require('./lib/trinte-subdomain.js');
